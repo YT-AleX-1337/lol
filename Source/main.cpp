6c459a795646dd8e2b927f97cfe4e62d915ab698
AleXandro <90471968+AleXandro-1337@users.noreply.github.com>
2023-02-02 21:05:52 +0100

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,21 @@
-////////////////////////////////////////////////
-/// JokeProgram.Win32_x64.lol.exe            ///
-/// Copyright (C) AleXandro_1337.            ///
-/// Licensed under GPL 3.0:                  ///
-/// http://www.gnu.org/licenses/gpl-3.0.html ///
-////////////////////////////////////////////////
+/*
+    JokeProgram.Win32_x64.lol.exe 
+    Joke program that BSoDs your computer. Save your work first!
+    Copyright (C) 2023 AleXandro_1337
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 
 typedef struct IUnknown IUnknown;
 
